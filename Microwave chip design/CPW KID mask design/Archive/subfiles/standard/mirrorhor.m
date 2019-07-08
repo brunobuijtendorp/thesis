eb3d1ea96cd4cf7255d9eca937e1e7c3892b939d
Bruno Buijtendorp <bruno@wlan-145-94-235-117.wlan.tudelft.nl>
2019-07-08 17:57:45 +0200

    add Jochems unchanged code to CPW KID mask design folder

--- Microwave chip design/CPW KID mask design/Archive/subfiles/standard/mirrorhor.m
+++
@@ -1,7 +0,0 @@
-
-function mirrorred=mirrorhor(ori)%mirrors around hzontal axis
-for i=1:size(ori,1)
-    mirrorred(i,1,:)=ori(i,1,:);
-    mirrorred(i,2,:)=-ori(i,2,:);
-end
-end
--- Microwave chip design/CPW KID mask design/Archive/subfiles/standard/plotkiddesign.m
+++
@@ -1,9 +0,0 @@
-
-function plotkiddesign(KID,color)
-for i=1:length(KID)
-    for j=1:size(KID{i},1)
-        plot(reshape(KID{i}(j,1,:),1,[]),reshape(KID{i}(j,2,:),1,[]),color);hold on;
-        hold on;
-    end
-end
-end
